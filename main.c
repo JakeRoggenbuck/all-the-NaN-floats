b47d39dfd261d33def8d9705f79b6e40575ecb16
Jake <jakeroggenbuck2@gmail.com>
2024-05-01 23:32:25 -0700

    Add correction for inf and -inf

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int main() {
     fp = fopen("all-the-nan-floats", "wb");
 
     // This is first number where all the exponent bits are set, but nothing
-    // else is set
-    unsigned int start = 0b011111111 << 23;
+    // else is set except the first bit, otherwise it would be infinity or zero
+    unsigned int start = (0b011111111 << 23) + 1;
 
     // This is number where all the bits have been set except the sign bit
     unsigned int end = (1 << 31) - 1u;
